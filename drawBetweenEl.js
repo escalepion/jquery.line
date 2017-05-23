4f0dd2d97d1b796ea3116257e684de778aee7b01
escalepion <escalepion@gmail.com>
2017-05-23 18:09:12 +0300

    changed just name drawBetweenEl

---
+++ drawBetweenEl.js
@@ -0,0 +1,17 @@
+function drawLineBetween (element1, element2) {
+    var offsetFirstElement = $(element1).offset();
+    var beginX = offsetFirstElement.left +$(element1).width() / 2;
+    var beginY = offsetFirstElement.top+ $(element1).height() / 2;
+    
+    var offsetSecondElement = $(element2).offset();
+    var endX = offsetSecondElement.left+ $(element2).width() / 2;
+    var endY = offsetSecondElement.top+ $(element2).height() / 2;
+
+    var coordinates = {
+        beginX : beginX,
+        beginY : beginY,
+        endX : endX,
+        endY : endY
+    }
+   $('#example').line(beginX, beginY, endX, endY, {color:"red", zindex:999});
+}
